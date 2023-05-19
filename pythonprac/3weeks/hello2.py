3b3c1e273a28569b7ec38c44779a8e1e0b380d1d
Kang-Gyeongwon <rosie02644@gmail.com>
2023-05-19 21:15:58 +0900

    chore: requests 라이브러리 사용해보기

--- pythonprac/3weeks/hello2.py
+++ pythonprac/3weeks/hello2.py
@@ -1,6 +1,13 @@
-import requests # requests 라이브러리 설치 필요
+import requests
 
 r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
 rjson = r.json()
 
-print(rjson)
+rows = rjson['RealtimeCityAir']['row']
+
+for a in rows:
+    # print(a)
+    
+    gu_name = a['MSRSTE_NM']
+    gu_miss = a['IDEX_MVL']
+    print(gu_name, gu_miss)
